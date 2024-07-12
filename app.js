bda860a5eb683f05cfc387b41fc6d34dcbf5215a
sburri <sburri@keypoint-tech.com>
2024-07-12 12:31:02 +0530

    db env changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// imports
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -11,13 +12,9 @@ var session = require('express-session');
 const flash = require('connect-flash');
 const { body, validationResult } = require('express-validator');
 
-const myLogger = function (req, res, next) {
-  console.log('LOGGED')
-  next()
-}
+
 
 var app = express();
-
 
 app.use(session({
   secret: 'secret234', 
@@ -26,9 +23,14 @@ app.use(session({
   cookie: { secure: false } 
 }));
 
-
 app.use(flash());
-// view engine setup
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success');
+  res.locals.error_messages = req.flash('error');
+  next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -40,13 +42,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success');
-  res.locals.error_messages = req.flash('error');
-  next();
-});
 
-
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
@@ -56,15 +53,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
-
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
